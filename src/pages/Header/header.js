753cd5a66ac08b9c9fe3fa3beb2c8ccd8a185858
Anmol <anmolsingh9898@gmail.com>
2019-08-17 18:48:15 -0400

    test semantic success

--- src/pages/Header/header.js
+++ src/pages/Header/header.js
@@ -7,36 +7,27 @@ class Header extends Component{
     render(){
         return (
     
-            <header className="header">
-                    <div className="headerContainer">
+            <header className="header ui grid">
+                    <div className="headerContainer menu">
                         <div className="logoContainer">
                             <img src={Logo} alt='logo'/>
                         </div>
-                        <div className="headingContainer">
-
-                        </div>
+                        
                         <div className="loginContainer"></div>
                      </div>
 
+                
                      <Menu>
 
                         <Menu.Menu>
-                            <Menu>My name</Menu>
+                            <Menu.Item>Pricing</Menu.Item>
+                            <Menu.Item>Sign Up</Menu.Item>
+                            <Menu.Item>Guest Login</Menu.Item>
                         </Menu.Menu>
 
                      </Menu>
-
-                    <nav className="">
-                        <ul className='nav'>
-                            <li className='nav-item'>Home</li>
-                            <li className='nav-item'>Features</li>
-                            <li className='nav-item'>Members</li>
-                            <li className='nav-item'>Targeting</li>
-                            <li className='nav-item'>How It Works</li>
-                            <li className='nav-item'>About US</li>
-                        </ul>
-                    </nav>
-
+                
+                   
             </header> 
         );
     }
