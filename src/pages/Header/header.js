d906bf56d25f976d109f73dcd0ddfacb5cbe02e7
Jaspreet Kaur <jaspreetr86@gmail.com>
2019-08-18 22:59:50 -0400

    update menu

--- src/pages/Header/header.js
+++ src/pages/Header/header.js
@@ -1,35 +1,45 @@
 import React, {Component} from 'react';
 import './header.css';
 import Logo from '../../images/go_for_it_LOGO.svg';
+import {Menu, Segment} from "semantic-ui-react";
 
  
 class Header extends Component{
-    render(){
+    state = { activeItem: 'home' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    render() {
+
+        const { activeItem } = this.state
         return (
-    
-            <header className="header">
-                    <div className="headerContainer">
-                        <div className="logoContainer">
-                            <img src={Logo} alt='logo'/>
-                        </div>
-                        <div className="headingContainer">
+            <div className="home-menu">
 
-                        </div>
-                        <div className="loginContainer"></div>
-                     </div>
+                <Segment inverted>
+                    <div className="logoContainer">
+                        <img src={Logo} alt='logo' />
+                    </div>
+                    <div className="menuContainer">
+                        <Menu inverted secondary>
+                            <Menu.Item
+                                name='Pricing'
+                                active={activeItem === 'Pricing'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                name='Sign Up'
+                                active={activeItem === 'Sign Up'}
+                                onClick={this.handleItemClick}
+                            />
+                            <Menu.Item
+                                name='Guest Login'
+                                active={activeItem === 'Guest Login'}
+                                onClick={this.handleItemClick}
+                            />
+                        </Menu>
+                    </div>
+                </Segment>
 
-                    <nav className="">
-                        <ul className='nav'>
-                            <li className='nav-item'>Home</li>
-                            <li className='nav-item'>Features</li>
-                            <li className='nav-item'>Members</li>
-                            <li className='nav-item'>Targeting</li>
-                            <li className='nav-item'>How It Works</li>
-                            <li className='nav-item'>About US</li>
-                        </ul>
-                    </nav>
-
-            </header> 
+            </div>
         );
     }
 }
